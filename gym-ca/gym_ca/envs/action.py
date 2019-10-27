19d5074891d07fda9f812d99e4f5d0eb23dc8b2d
Alex <ajoerger@mit.edu>
2019-10-27 15:10:46 -0400

    restricted movement of intruder to 'not go backwards'

--- gym-ca/gym_ca/envs/action.py
+++ gym-ca/gym_ca/envs/action.py
@@ -34,6 +34,36 @@ def act(pos, a, n, m):
         raise Exception('Invalid action.')
 
 
+def act_intr(pos, a, n, m, a_last):
+    a = a_str(a)
+    x, y = pos
+
+    if a == 'NOOP':
+        return AC_State(x,y), a_last
+    elif a == 'UP':
+        if a_last == 'DOWN':
+            return AC_State(x,y), a_last
+        else:
+            return AC_State(x, y+1) if y+1 < n else pos, a
+    elif a == 'DOWN':
+        if a_last == 'UP':
+            return AC_State(x,y), a_last
+        else:
+            return AC_State(x, y-1) if 0 <= y-1 else pos, a
+    elif a == 'LEFT':
+        if a_last == 'RIGHT':
+            return AC_State(x,y), a_last
+        else:
+            return AC_State(x-1, y) if 0 <= x-1 else pos, a
+    elif a == 'RIGHT':
+        if a_last == 'LEFT':
+            return AC_State(x,y), a_last
+        else:
+            return AC_State(x+1, y) if x+1 < m else pos, a
+    else:
+        raise Exception('Invalid action.')
+
+
 def act_obstacles(pos, a, grid):
     n, m, obstacles = grid
     new_pos = act(pos, a, n, m)
