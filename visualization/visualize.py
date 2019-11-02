c4ca71f813bbef9bd2baf353eddb1bb905a5e295
Mohammad <moislam@mit.edu>
2019-11-02 15:11:54 -0400

    Enabled visualize.py to read from script to update position of the aircraft using keys

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -4,15 +4,23 @@ import numpy as np
 pyglet.resource.path = ["images"]
 pyglet.resource.reindex()
 from pyglet.window import key
+import pylab
+
+def computeGridCorners(windowPixels_x, windowPixels_y, Nx, Ny):
+
+  # Compute corners of grid world
+  leftX = (1.0/4.0) * windowPixels_x
+  rightX = (3.0/4.0) * windowPixels_x
+  topY = (3.0/4.0) * windowPixels_y
+  bottomY = (1.0/4.0) * windowPixels_y
+
+  return leftX, rightX, topY, bottomY, 
 
 # Grid world function
 def drawGridWorld(windowPixels_x, windowPixels_y, Nx, Ny):
 
   # Compute appropriate corners for our grid world, so that it lies sufficiently inside the window
-  leftX = (2.0/8.0) * windowPixels_x
-  rightX = (6.0/8.0) * windowPixels_x
-  topY = (6.0/8.0) * windowPixels_y
-  bottomY = (2.0/8.0) * windowPixels_y
+  leftX, rightX, topY, bottomY = computeGridCorners(windowPixels_x, windowPixels_y, Nx, Ny)
 
   glBegin(GL_LINES)
   
@@ -44,15 +52,11 @@ def drawGridWorld(windowPixels_x, windowPixels_y, Nx, Ny):
 def initializeAircraft(windowPixels_x, windowPixels_y, Nx, Ny):
   
   # Compute appropriate corners for our grid world, so that it lies sufficiently inside the window
-  leftX = (2.0/8.0) * windowPixels_x
-  rightX = (6.0/8.0) * windowPixels_x
-  topY = (6.0/8.0) * windowPixels_y
-  bottomY = (2.0/8.0) * windowPixels_y
-
+  leftX, rightX, topY, bottomY = computeGridCorners(windowPixels_x, windowPixels_y, Nx, Ny)
   dx = (rightX - leftX) / Nx
   dy = (topY - bottomY) / Ny
   
-  # Place aircraft on screen
+  # Create aircraft object
   aircraft = pyglet.resource.image("aircraft.png") 
   aircraft.anchor_x = aircraft.width/2
   aircraft.anchor_y = aircraft.height/2
@@ -65,11 +69,7 @@ def initializeAircraft(windowPixels_x, windowPixels_y, Nx, Ny):
 def initializeObstacle(windowPixels_x, windowPixels_y, Nx, Ny):
   
   # Compute appropriate corners for our grid world, so that it lies sufficiently inside the window
-  leftX = (2.0/8.0) * windowPixels_x
-  rightX = (6.0/8.0) * windowPixels_x
-  topY = (6.0/8.0) * windowPixels_y
-  bottomY = (2.0/8.0) * windowPixels_y
-
+  leftX, rightX, topY, bottomY = computeGridCorners(windowPixels_x, windowPixels_y, Nx, Ny)
   dx = (rightX - leftX) / Nx
   dy = (topY - bottomY) / Ny
   
@@ -98,20 +98,57 @@ game_window.push_handlers(keys)
 aircraft_sprite = initializeAircraft(windowPixels_x, windowPixels_y, Nx, Ny)
 obstacle_sprite = initializeObstacle(windowPixels_x, windowPixels_y, Nx, Ny)
 
+filename = "position.txt"
+
+data = pylab.loadtxt(open(filename), delimiter=' ', usecols=(0,1))
+maxMoves = len(data[:,0])
+print(maxMoves)
+def readPosition(data, moveNumber):
+  i = data[moveNumber, 0]
+  j = data[moveNumber, 1]
+
+  return i, j
+
+def moveAircraft(windowPixels_x, windowPixels_y, i, j):
+  # Compute appropriate corners for our grid world, so that it lies sufficiently inside the window
+  leftX, rightX, topY, bottomY = computeGridCorners(windowPixels_x, windowPixels_y, Nx, Ny)
+  dx = (rightX - leftX) / Nx
+  dy = (topY - bottomY) / Ny
+  
+  # Compute new x position and y position of aircraft based on index
+  x_pos = leftX + 0.5 * dx + i * dx
+  y_pos = bottomY + 0.5 * dy + j * dy
+  
+  # Update aircraft position properties to match new position
+  aircraft_sprite.x = x_pos
+  aircraft_sprite.y = y_pos
+  
+moveNumber = 0
 # Call update function every cycle to detect key inputs and move accordingly
 def update(dt):
+  global moveNumber
+  i, j = readPosition(data, moveNumber)
+  moveAircraft(windowPixels_x, windowPixels_y, i, j)
+  print(moveNumber)
+  if (keys[key.RIGHT] and moveNumber < maxMoves-1):
+    moveNumber = moveNumber + 1 
+  if (keys[key.LEFT] and moveNumber > 0):
+    moveNumber = moveNumber - 1
+ 
+# Call update function every cycle to detect key inputs and move accordingly
+def updateOld(dt):
   if keys[key.LEFT]:
     aircraft_sprite.x = aircraft_sprite.x - 100*dt
     aircraft_sprite.rotation = 180
   if keys[key.RIGHT]:
-	aircraft_sprite.x = aircraft_sprite.x + 100*dt
-	aircraft_sprite.rotation = 0
+    aircraft_sprite.x = aircraft_sprite.x + 100*dt
+    aircraft_sprite.rotation = 0
   if keys[key.UP]:
-	aircraft_sprite.y = aircraft_sprite.y + 100*dt
-	aircraft_sprite.rotation = 270
+    aircraft_sprite.y = aircraft_sprite.y + 100*dt
+    aircraft_sprite.rotation = 270
   if keys[key.DOWN]:
-	aircraft_sprite.y = aircraft_sprite.y - 100*dt
-	aircraft_sprite.rotation = 90
+    aircraft_sprite.y = aircraft_sprite.y - 100*dt
+    aircraft_sprite.rotation = 90
 
 @game_window.event
 def on_draw():
@@ -124,12 +161,14 @@ def on_draw():
   
   # Place aircraft on lower lefthand side of screen (can change later)
   aircraft_sprite.draw()
+
   # Place enemy on lower righthand side of screen (can change later)
   obstacle_sprite.draw()
+
   # Create title of the environment (Aircraft Collision Avoidance Environment)
   level_label = pyglet.text.Label('Aircraft Collision Avoidance Environment', font_size=24, x = windowPixels_x/2, y = windowPixels_y-100, anchor_x='center')
   level_label.draw()
 
 # Run update function to update position of airplane every cycle 
-pyglet.clock.schedule_interval(update, 1/120.0)
+pyglet.clock.schedule_interval(update, 1/30.0)
 pyglet.app.run()
