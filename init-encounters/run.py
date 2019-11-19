cb2629b2ced33a06b62d0cea9b159058463acb42
Mohammad Shafaet Islam <mohammad@AIRFOIL.MIT.EDU>
2019-11-19 00:13:16 -0500

    Now run.py outputs text file with aircraft and intruder positions to visualization folder

--- init-encounters/run.py
+++ init-encounters/run.py
@@ -24,11 +24,16 @@ def run(load_path, env_name='gym_ca:ca-gridworld-v0'):
         obs, done = env.reset(), False
 
         last_act_intr = 'NOOP'
+        position_file = open("../visualization/position.txt","a") 
         while not done:
             env.render()
             time.sleep(.2)
-            obs, _, done, _, last_act_intr = env.step(policy([obs])[0],last_act_intr)
+            obs, _, done, pos, last_act_intr = env.step(policy([obs])[0],last_act_intr)
+            print(str(obs))
+            position_file.write(str(obs[0]) + " " + str(obs[1]) + " " + str(obs[2]) + " " + str(obs[3]) + "\n")
+           
+        position_file.close() 
 
 
 if __name__ == '__main__':
-    run('gridworld_policy_1572201951.pkl')
+    run('gridworld_policy_1574112290.pkl')
